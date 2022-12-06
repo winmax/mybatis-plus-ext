2c80f8dbf8051884a599e1783922f731873086ad
tangzc <imtzc@qq.com>
2022-12-06 09:32:17 +0800

    修复bug：根据class和字段名获取字段的方法，导致获取不到父类的对应字段，返回null的问题。

--- mybatis-plus-ext-core/mybatis-plus-ext-magic/src/main/java/com/tangzc/mpe/magic/BeanClassUtil.java
+++ mybatis-plus-ext-core/mybatis-plus-ext-magic/src/main/java/com/tangzc/mpe/magic/BeanClassUtil.java
@@ -1,4 +1,4 @@
-package com.tangzc.mpe.magic;
+package com.tangzc.mpe.base.util;
 
 import com.baomidou.mybatisplus.core.toolkit.LambdaUtils;
 import com.baomidou.mybatisplus.core.toolkit.support.LambdaMeta;
@@ -14,13 +14,32 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BeanClassUtil {
 
     public static <E> String getFieldName(SFunction<E, ?> sf) {
         LambdaMeta lambda = LambdaUtils.extract(sf);
         return PropertyNamer.methodToProperty(lambda.getImplMethodName());
+    }
+
+    public static Map<String, Object> beanToMap(Object bean) {
+
+        Map<String, Object> retMap = new HashMap<>();
+
+        Class<?> beanClass = bean.getClass();
+        List<Field> allDeclaredFields = getAllDeclaredFields(beanClass);
+
+        try {
+            for (Field field : allDeclaredFields) {
+                retMap.put(field.getName(), BeanClassUtil.getReadMethod(beanClass, field).invoke(bean));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return retMap;
     }
 
     public static Class<?> getFieldRealClass(Field field) {
@@ -40,12 +59,23 @@ public class BeanClassUtil {
     public static <T> Class<?> getFieldRealClass(Class<T> clazz, SFunction<T, ?> sFunction) {
 
         String fieldName = getFieldName(sFunction);
-        Field field = getField(clazz, fieldName);
-        if (field == null) {
+        Field field;
+        try {
+            field = clazz.getField(fieldName);
+        } catch (NoSuchFieldException e) {
             throw new RuntimeException(clazz + "下未找到" + fieldName + "字段");
         }
 
-        return getFieldRealClass(field);
+        Class<?> fieldClass = field.getType();
+        if (Collection.class.isAssignableFrom(fieldClass)) {
+            // 如果是集合，获取其泛型参数class
+            Type genericType = field.getGenericType();
+            if (genericType instanceof ParameterizedType) {
+                ParameterizedType pt = (ParameterizedType) genericType;
+                fieldClass = (Class<?>) pt.getActualTypeArguments()[0];
+            }
+        }
+        return fieldClass;
     }
 
     public static Method getWriteMethod(Class<?> clazz, Field field) {
@@ -82,29 +112,11 @@ public class BeanClassUtil {
         throw new RuntimeException(clazz + "下未找到" + fieldName + "的get/is方法");
     }
 
-    /**
-     * 查找类下指定的字段，如果当前类没有，那就去它的父类寻找
-     * @param clazz 类
-     * @param fieldName 字段名
-     * @return 字段
-     */
     public static Field getField(Class<?> clazz, String fieldName) {
 
-        while (true) {
-            Field field = Arrays.stream(clazz.getDeclaredFields())
-                    .filter(f -> f.getName().equals(fieldName))
-                    .findFirst().orElse(null);
-            // 如果没有找到
-            if (field == null) {
-                Class<?> superclass = clazz.getSuperclass();
-                // 如果存在父类，且不是Object，则去上一级的父类继续寻找
-                if (superclass != null && superclass != Object.class) {
-                    clazz = superclass;
-                    continue;
-                }
-            }
-            return field;
-        }
+        return Arrays.stream(clazz.getDeclaredFields())
+                .filter(field -> field.getName().equals(fieldName))
+                .findFirst().orElse(null);
     }
 
     public static List<Field> getAllDeclaredFields(Class<?> beanClass) {
