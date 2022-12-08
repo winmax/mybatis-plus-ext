d3b677a3d7cfc5431008286717d17574238ff53d
tangzc <imtzc@qq.com>
2022-12-09 00:09:44 +0800

    完善SQLite策略适配

--- mybatis-plus-ext-core/mybatis-plus-ext-magic/src/main/java/com/tangzc/mpe/magic/BeanClassUtil.java
+++ mybatis-plus-ext-core/mybatis-plus-ext-magic/src/main/java/com/tangzc/mpe/magic/BeanClassUtil.java
@@ -1,4 +1,4 @@
-package com.tangzc.mpe.base.util;
+package com.tangzc.mpe.magic;
 
 import com.baomidou.mybatisplus.core.toolkit.LambdaUtils;
 import com.baomidou.mybatisplus.core.toolkit.support.LambdaMeta;
@@ -11,35 +11,14 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BeanClassUtil {
 
     public static <E> String getFieldName(SFunction<E, ?> sf) {
         LambdaMeta lambda = LambdaUtils.extract(sf);
         return PropertyNamer.methodToProperty(lambda.getImplMethodName());
-    }
-
-    public static Map<String, Object> beanToMap(Object bean) {
-
-        Map<String, Object> retMap = new HashMap<>();
-
-        Class<?> beanClass = bean.getClass();
-        List<Field> allDeclaredFields = getAllDeclaredFields(beanClass);
-
-        try {
-            for (Field field : allDeclaredFields) {
-                retMap.put(field.getName(), BeanClassUtil.getReadMethod(beanClass, field).invoke(bean));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return retMap;
     }
 
     public static Class<?> getFieldRealClass(Field field) {
@@ -59,23 +38,12 @@ public class BeanClassUtil {
     public static <T> Class<?> getFieldRealClass(Class<T> clazz, SFunction<T, ?> sFunction) {
 
         String fieldName = getFieldName(sFunction);
-        Field field;
-        try {
-            field = clazz.getField(fieldName);
-        } catch (NoSuchFieldException e) {
+        Field field = getField(clazz, fieldName);
+        if (field == null) {
             throw new RuntimeException(clazz + "下未找到" + fieldName + "字段");
         }
 
-        Class<?> fieldClass = field.getType();
-        if (Collection.class.isAssignableFrom(fieldClass)) {
-            // 如果是集合，获取其泛型参数class
-            Type genericType = field.getGenericType();
-            if (genericType instanceof ParameterizedType) {
-                ParameterizedType pt = (ParameterizedType) genericType;
-                fieldClass = (Class<?>) pt.getActualTypeArguments()[0];
-            }
-        }
-        return fieldClass;
+        return getFieldRealClass(field);
     }
 
     public static Method getWriteMethod(Class<?> clazz, Field field) {
@@ -112,11 +80,29 @@ public class BeanClassUtil {
         throw new RuntimeException(clazz + "下未找到" + fieldName + "的get/is方法");
     }
 
+    /**
+     * 查找类下指定的字段，如果当前类没有，那就去它的父类寻找
+     * @param clazz 类
+     * @param fieldName 字段名
+     * @return 字段
+     */
     public static Field getField(Class<?> clazz, String fieldName) {
 
-        return Arrays.stream(clazz.getDeclaredFields())
-                .filter(field -> field.getName().equals(fieldName))
-                .findFirst().orElse(null);
+        while (true) {
+            Field field = Arrays.stream(clazz.getDeclaredFields())
+                    .filter(f -> f.getName().equals(fieldName))
+                    .findFirst().orElse(null);
+            // 如果没有找到
+            if (field == null) {
+                Class<?> superclass = clazz.getSuperclass();
+                // 如果存在父类，且不是Object，则去上一级的父类继续寻找
+                if (superclass != null && superclass != Object.class) {
+                    clazz = superclass;
+                    continue;
+                }
+            }
+            return field;
+        }
     }
 
     public static List<Field> getAllDeclaredFields(Class<?> beanClass) {
@@ -129,10 +115,18 @@ public class BeanClassUtil {
     private static void getFieldList(List<Field> fields, Class<?> beanClass) {
 
         Field[] declaredFields = beanClass.getDeclaredFields();
-        fields.addAll(Arrays.asList(declaredFields));
+        // 获取当前class的所有fields的name列表
+        Set<String> fieldNames = fields.stream().map(Field::getName).collect(Collectors.toSet());
+        for (Field field : declaredFields) {
+            // 避免重载属性
+            if (fieldNames.contains(field.getName())) {
+                continue;
+            }
+            fields.add(field);
+        }
 
         Class<?> superclass = beanClass.getSuperclass();
-        if (superclass != null && superclass != Object.class) {
+        if (superclass != null) {
             getFieldList(fields, superclass);
         }
     }
